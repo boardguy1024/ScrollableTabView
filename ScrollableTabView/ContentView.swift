7447770b47394c31e37f545b338423537bde042b
boardguy1024 <boardguy1024@gmail.com>
2023-11-18 13:22:02 +0900

    Initial Commit

---
+++ ScrollableTabView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScrollableTabView
+//
+//  Created by park kyung seok on 2023/11/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScrollableTabView/ScrollableTabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollableTabViewApp.swift
+//  ScrollableTabView
+//
+//  Created by park kyung seok on 2023/11/18.
+//
+
+import SwiftUI
+
+@main
+struct ScrollableTabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
